b7d3203d12db531d1fc0adbbb61a894e7d4efd05
Vishal Mejari <vishalmejari@Vishals-MacBook-Pro.local>
2026-06-25 14:12:14 +0530

    Update web build for mobile app

---
+++ dist/assets/index-DYHVRazm.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DdtFXIvb.js","assets/index-xlKnbI6E.js","assets/index-DYVBK5EZ.css"])))=>i.map(i=>d[i]);
+import{r,_ as i}from"./index-xlKnbI6E.js";const _=r("PrivacyScreen",{web:()=>i(()=>import("./web-DdtFXIvb.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PrivacyScreenWeb)});export{_ as PrivacyScreen};
